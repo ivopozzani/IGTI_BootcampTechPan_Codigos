4d974331b288d4591fe1340d05745004cb117489
ivopozzani <ivo.pozzani@gmail.com>
2021-09-16 21:51:07 -0300

    progress

--- TrabPratMod1/main.js
+++ TrabPratMod1/main.js
@@ -1,14 +1,27 @@
 const submit = document.querySelector('form')
 
 function handleInterestRate () {
+  const prest = document.querySelectorAll('table tr')
   const valor = Number(document.querySelector('#valor').value)
   const prazoI = Number(document.querySelector('#prazoI').value)
   const jurosI = Number(document.querySelector('#jurosI').value)
-  
+  const amort1 = 240
   const result = valor*((1 + jurosI)**prazoI)
   
- 
-  console.log(result)
+const resultAmort = result/amort1
+
+function tableFill () {
+  if (u === 1) return resultAmort;
+  if (u === 2) return "hi";
+  if (u === 3) return "worked again";
+}
+
+ for (var i = 1; i <= 5; i++) {
+  for (var u = 1; u <= 3; u++){
+    prest[i].children[u].textContent = tableFill ()
+    
+  } }
+
   console.log(valor)
   console.log(prazoI)
   console.log(jurosI)
@@ -16,3 +29,6 @@ function handleInterestRate () {
 }
 
 submit.addEventListener('submit', handleInterestRate)
+//const prest = document.querySelectorAll('table tr')
+
+//console.log(prest[1])
