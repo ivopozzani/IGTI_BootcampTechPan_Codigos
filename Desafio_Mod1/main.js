f803ba109adf30b25867d8fe059ba8d1c52a5199
ivopozzani <ivo.pozzani@gmail.com>
2021-09-23 20:02:36 -0300

    update

--- Desafio_Mod1/main.js
+++ Desafio_Mod1/main.js
@@ -1,51 +1,58 @@
 const checkBox = document.querySelectorAll('.filterOptions input')
-let finalchecked = undefined
-function handleCheck () {
-  const checked = document.querySelectorAll('.filterOptions input:checked')
+let checkedList = [1]
+
+
+// let employeesPromise = fetch("http://localhost:3000/employees");
+
+// employeesPromise.then((resp) => {resp.json().then((employees) => {
+//   let rolesPromise = fetch("http://localhost:3000/roles");
+//   rolesPromise.then((r) => {r.json().then((roles) => {
+//    let table = myTable(employees, roles);
+//    document.querySelector('.app').innerHTML = table
+   
+//   })});  
+//   })})
   
-  var checkedList = []
-  for (let i = 0 ; i < checked.length ; i++) checkedList.push(checked[i].attributes.roleid.value)
+  function handleCheck () {
+    const checked = document.querySelectorAll('.filterOptions input:checked')
+    checkedList = []
+    for (let i = 0 ; i < checked.length ; i++) checkedList.push(checked[i].attributes['data-roleid'].value)
+    
+    return checkedList}
   
-  finalchecked = checkedList
-  console.log(finalchecked)
-}
+  function solution3(){
+    let empPromise = fetchJSON("http://localhost:3000/employees");
+    let rolesPromise = fetchJSON("http://localhost:3000/roles");
+    Promise.all([empPromise, rolesPromise]).then((result) => {
+      let employees = result[0];
+      let roles = result[1];
+      let table = myTable(employees, roles);
+      document.querySelector('.app').innerHTML = table;
+    })
+  }
+
+solution3();
 
 
-checkBox.forEach((check) => check.addEventListener('change', handleCheck))
-console.log(checkBox)
+  function myTable (employees, roles) {
+    
+    // var test = [1,4]
+    // var filtered = employees.filter((employee) => {return test[0] == employee.role_id || checkedList[3] == employee.role_id})
+      
+    //  console.log(filtered)
+      
+      let rows = employees.map((employee) => {
+          let role = roles.find(role => role.id == employee.role_id);
+        
+          return `<tr><td>${employee.id}</td><td>${employee.name}</td><td>${role.name}</td><td>${employee.salary}</td></tr>`})
+        
+        return `<table>${rows.join("")}</table>`;
+      }
 
+      
 
-
-
-
-
-
-
-
-
-
-let employeesPromise = fetch("http://localhost:3000/employees");
-
-employeesPromise.then((resp) => {resp.json().then((employees) => {
-  let rolesPromise = fetch("http://localhost:3000/roles");
-  rolesPromise.then((r) => {r.json().then((roles) => {
-    let table = myTable(employees, roles);
-    document.querySelector('.app').innerHTML = table})});  
-  })})
-
-
-function myTable (employees, roles) {
-
-  var test = [1,4]
-
-var filtered = employees.filter((employee) => {return test[0] == employee.role_id || test[3] == employee.role_id})
-
-console.log(filtered)
-
-// let rows = employees.map((employee) => {
-//   let role = roles.find(role => role.id == employee.role_id);
-  
-//   return `<tr><td>${employee.id}</td><td>${employee.name}</td><td>${role.name}</td><td>${employee.salary}</td></tr>`})
-
-// return `<table>${rows.join("")}</table>`;
-}
+      // checkBox.forEach((check) => check.addEventListener('change', () =>{
+      //   handleCheck();
+      //   myTable();
+      // }))
+      
