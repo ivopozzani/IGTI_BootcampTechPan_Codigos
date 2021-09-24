7d15784afc98801905b05805f35605814618488f
ivopozzani <ivo.pozzani@gmail.com>
2021-09-24 19:54:47 -0300

    update

--- Desafio_Mod1/main.js
+++ Desafio_Mod1/main.js
@@ -1,58 +1,63 @@
+const sort = document.querySelector("#sort")
 const checkBox = document.querySelectorAll('.filterOptions input')
-let checkedList = [1]
+let employeeslist = []
+let roleslist = []
 
 
-// let employeesPromise = fetch("http://localhost:3000/employees");
-
-// employeesPromise.then((resp) => {resp.json().then((employees) => {
-//   let rolesPromise = fetch("http://localhost:3000/roles");
-//   rolesPromise.then((r) => {r.json().then((roles) => {
-//    let table = myTable(employees, roles);
-//    document.querySelector('.app').innerHTML = table
-   
-//   })});  
-//   })})
+function fetchJson(url) {
+ return fetch(url).then((r) => {
+    return r.json();
+  });
+}
   
-  function handleCheck () {
-    const checked = document.querySelectorAll('.filterOptions input:checked')
-    checkedList = []
-    for (let i = 0 ; i < checked.length ; i++) checkedList.push(checked[i].attributes['data-roleid'].value)
-    
-    return checkedList}
+async function HttpRequest(){
+  let [employees, roles] = await Promise.all([
+    fetchJson("http://localhost:3000/employees"),
+    fetchJson("http://localhost:3000/roles")
+  ]);
+  employeeslist = employees
+  roleslist = roles    
+  let table = myTable(employeeslist, roleslist);
+  document.querySelector('.app').innerHTML = table;
+}
+
+function myTable (employees, roles) {
   
-  function solution3(){
-    let empPromise = fetchJSON("http://localhost:3000/employees");
-    let rolesPromise = fetchjson("http://localhost:3000/roles");
-    Promise.all([empPromise, rolesPromise]).then((result) => {
-      let employees = result[0];
-      let roles = result[1];
-      let table = myTable(employees, roles);
-      document.querySelector('.app').innerHTML = table;
-    })
-  }
+  const checked = document.querySelectorAll('.filterOptions input:checked')
+  const checkedList = []
+  let filtered = []
+  
 
-solution3();
-
-
-  function myTable (employees, roles) {
+  for (let i = 0 ; i < checked.length ; i++) checkedList.push(checked[i].attributes['data-roleid'].value)
     
-    // var test = [1,4]
-    // var filtered = employees.filter((employee) => {return test[0] == employee.role_id || checkedList[3] == employee.role_id})
-      
-    //  console.log(filtered)
-      
-      let rows = employees.map((employee) => {
-          let role = roles.find(role => role.id == employee.role_id);
-        
-          return `<tr><td>${employee.id}</td><td>${employee.name}</td><td>${role.name}</td><td>${employee.salary}</td></tr>`})
-        
-        return `<table>${rows.join("")}</table>`;
-      }
+  
+  filtered = employees.filter((employee) => {return checkedList[0] == employee.role_id || checkedList[1] == employee.role_id || checkedList[2] == employee.role_id || checkedList[3] == employee.role_id || checkedList[4] == employee.role_id || checkedList[5] == employee.role_id || checkedList[6] == employee.role_id || checkedList[7] == employee.role_id || checkedList[8] == employee.role_id || checkedList[9] == employee.role_id || checkedList[10] == employee.role_id || checkedList[11] == employee.role_id})
+  
 
-      
+  if (sort.value === 'Nome descendente') {filtered.sort((a, b) => {
+    if (a.name > b.name) {return -1}
+  })} else if (sort.value === 'Salário ascendente') {filtered.sort((a, b) => {
+    if (a.salary < b.salary) {return -1}
+  })} else if (sort.value === 'Salário descendente') {filtered.sort((a, b) => {
+    if (a.salary > b.salary) {return -1}
+  })} else {filtered.sort((a, b) => {
+    if (a.name < b.name) {return -1}
+  })}
+  
+  let rows = filtered.map((employee) => {
+    let role = roles.find(role => role.id == employee.role_id);        
+    return `<tr><td>${employee.id}</td><td>${employee.name}</td><td>${role.name}</td><td>${employee.salary}</td></tr>`})
+        
+  return `<table>${rows.join("")}</table>`;
+}
 
-      // checkBox.forEach((check) => check.addEventListener('change', () =>{
-      //   handleCheck();
-      //   myTable();
-      // }))
-      
+function tableToHTML () {
+  let table = myTable(employeeslist, roleslist);
+  document.querySelector('.app').innerHTML = table;
+}
+
+checkBox.forEach((check) => check.addEventListener('change', tableToHTML))
+
+sort.addEventListener('change', tableToHTML)
+
+HttpRequest();
