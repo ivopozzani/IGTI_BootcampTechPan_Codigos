6101a572a5b06d36f51d101202c193e4346b08c9
ivopozzani <ivo.pozzani@gmail.com>
2021-10-10 23:15:18 -0300

    update

--- Desafio_Mod2_Angular/ngFood/src/app/app.module.ts
+++ Desafio_Mod2_Angular/ngFood/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { PedidoComponent } from './pedido/pedido.component';
 import { CardapioComponent } from './cardapio/cardapio.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [AppComponent, PedidoComponent, CardapioComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
