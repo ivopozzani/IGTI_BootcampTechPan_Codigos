e0840ffb89a03ff469a2d7e9c71b328c8df8ed9e
ivopozzani <ivo.pozzani@gmail.com>
2021-11-08 14:27:24 -0300

    New Project (Vendas) created

---
+++ Exercicios_Java/vendas/src/test/java/com/pozzani/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pozzani.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
