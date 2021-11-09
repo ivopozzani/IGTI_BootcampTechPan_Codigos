05ba25fa89f208ddbfcbc74d89d1dc9688e023f6
ivopozzani <ivo.pozzani@gmail.com>
2021-11-09 18:35:01 -0300

    Update ClienteController.java
    Controller was changed to RestController

--- Exercicios_Java/vendas/src/main/java/com/pozzani/vendas/controllers/ClienteController.java
+++ Exercicios_Java/vendas/src/main/java/com/pozzani/vendas/controllers/ClienteController.java
@@ -1,14 +1,15 @@
 package com.pozzani.vendas.controllers;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
-	@GetMapping
+	@GetMapping("")
 	public String test() {
 		return "ok";
 	}
