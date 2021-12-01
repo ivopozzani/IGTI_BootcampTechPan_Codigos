f59baa9e6dd124a1c0542d43096ba8ab9ade8ffe
ivopozzani <ivo.pozzani@gmail.com>
2021-12-01 07:55:35 -0300

    Update PedidoController.java
    dynamic endpoint created

--- DesafioFinal/desafio-final-backend/src/main/java/igti/desafio/controller/PedidoController.java
+++ DesafioFinal/desafio-final-backend/src/main/java/igti/desafio/controller/PedidoController.java
@@ -3,6 +3,8 @@ package igti.desafio.controller;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,10 @@ public class PedidoController {
 		
 	}
 		
+	@GetMapping("/pedidos/{idPedido}")
+	public Pedido obtemPedido(@PathVariable Integer idPedido) {
+		return pedidoRepository.getById(idPedido);
 		
+	}
 
 }
